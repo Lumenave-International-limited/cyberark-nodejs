b12da2202811b603d99f3448df91a9019feb98d6
harrytimbog <harriemannie@gmail.com>
2023-06-21 08:41:36 +0100

    Persist User in Local Storage

--- client/app/context/appContext.js
+++ client/app/context/appContext.js
@@ -12,13 +12,16 @@ import {
   REGISTER_USER_ERROR,
 } from "./actions";
 
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
+  user: user ? JSON.parse(user) : null,
+  token: token,
 };
 
 const AppContext = React.createContext({});
@@ -37,6 +40,16 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({ user, token }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
 
@@ -50,6 +63,8 @@ const AppProvider = ({ children }) => {
       dispatch({ type: REGISTER_USER_SUCCESS, payload: { user, token } });
 
       // local storage later
+
+      addUserToLocalStorage({ user, token });
     } catch (error) {
       console.log(error.response);
       dispatch({
